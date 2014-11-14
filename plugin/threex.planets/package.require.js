3c360fcb03a06ee55d155e8b903e5d01b05a75bc
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-11-14 13:46:01 +0100

    added threex.planets to plugins

---
+++ plugin/threex.planets/package.require.js
@@ -0,0 +1,8 @@
+define( [ 'module'
+	, './threex.planets.js',
+	, './threex.atmospherematerial'
+	, './threex.atmospheredatgui'
+	], function(module){
+	// set baseUrl for this plugin
+	THREEx.Planets.baseURL	= module.uri+'/../';
+});
